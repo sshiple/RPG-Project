a8174265a9894df01e8e9818f14ca09954d12b8f
sshiple <124829377+sshiple@users.noreply.github.com>
2023-03-29 15:43:23 -0400

    Add files via upload
    Added temporary sprites and added a simple jump movement to the player

--- rpg.py
+++ rpg.py
@@ -9,7 +9,23 @@ pygame.display.set_caption("RPG")
 
 #Game Area
 board = pygame.Rect(0, 0, screenWidth, screenHeight)
+battleground = pygame.Rect(0, screenHeight/2, screenWidth, screenHeight)
 
+#Player Creation
+playerSize = playerWidth, playerHeight = 20, 20
+playerPos = playerX, playerY = screenWidth/5, 3*screenHeight/5
+player = pygame.Rect(playerX, playerY, playerWidth, playerHeight)
+
+#Enemy Creation
+enemySize = enemyWidth, enemyHeight = 20, 20
+enemyPos = enemyX, enemyY = 4*screenWidth/5, 3*screenHeight/5
+enemy = pygame.Rect(enemyX, enemyY, enemyWidth, enemyHeight)
+
+#Player Movement
+def playerMove(player):
+    key = pygame.key.get_pressed()
+    if key[pygame.K_w]:
+        player.move_ip(0, -5)
 
 #Game Loop
 #Using "while 1" instead of "while True" is more efficient
@@ -19,6 +35,12 @@ while 1:
             pygame.quit()
             sys.exit()
 
-    surface.fill((36, 36, 36)) #Temporary background
+    surface.fill((0, 0, 125)) #Temporary background
+
+    playerMove(player)
+
+    pygame.draw.rect(surface, (0, 125, 0), battleground)
+    pygame.draw.rect(surface, (255, 255, 255), player)
+    pygame.draw.rect(surface, (255, 0, 0), enemy)
 
     pygame.display.update(board)
