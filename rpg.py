f2183f030fa2f9f3b063b9e93576621ad38380f5
sshiple <124829377+sshiple@users.noreply.github.com>
2023-03-30 23:35:47 -0400

    Add files via upload
    Updated the player's jump

--- rpg.py
+++ rpg.py
@@ -13,19 +13,30 @@ battleground = pygame.Rect(0, screenHeight/2, screenWidth, screenHeight)
 
 #Player Creation
 playerSize = playerWidth, playerHeight = 20, 20
-playerPos = playerX, playerY = screenWidth/5, 3*screenHeight/5
+playerPos = playerX, playerY = 224, 378 #screenWidth/5, 3*screenHeight/5
 player = pygame.Rect(playerX, playerY, playerWidth, playerHeight)
+jumpHeight = 5
 
 #Enemy Creation
 enemySize = enemyWidth, enemyHeight = 20, 20
 enemyPos = enemyX, enemyY = 4*screenWidth/5, 3*screenHeight/5
 enemy = pygame.Rect(enemyX, enemyY, enemyWidth, enemyHeight)
 
+
 #Player Movement
 def playerMove(player):
+    global playerY
     key = pygame.key.get_pressed()
-    if key[pygame.K_w]:
-        player.move_ip(0, -5)
+    jumped = 0
+
+    #Jump
+    if key[pygame.K_w] and playerY == 378:
+        player.move_ip(0, -50)
+        playerY -= 50
+    elif playerY < 378: #w shouldn't be pressed again in the air
+        player.move_ip(0, 1)
+        playerY += 1
+
 
 #Game Loop
 #Using "while 1" instead of "while True" is more efficient
