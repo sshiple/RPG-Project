1b6a2c7b5fdd1dc249d9b53a5d24c1ffdab29adc
sshiple <124829377+sshiple@users.noreply.github.com>
2023-04-12 20:14:41 -0400

    Add files via upload

--- rpg.py
+++ rpg.py
@@ -11,12 +11,18 @@ pygame.display.set_caption("RPG")
 board = pygame.Rect(0, 0, screenWidth, screenHeight)
 battleground = pygame.Rect(0, screenHeight/2, screenWidth, screenHeight)
 
-#Player Creation
-playerSize = playerWidth, playerHeight = 20, 20
-playerPos = playerX, playerY = 224, 378 #screenWidth/5, 3*screenHeight/5
-player = pygame.Rect(playerX, playerY, playerWidth, playerHeight)
+#character Creation
+characterSize = characterWidth, characterHeight = 20, 20
+
+character1Pos = character1X, character1Y = 224, 378 #screenWidth/5, 3*screenHeight/5
+character1 = pygame.Rect(character1X, character1Y, characterWidth, characterHeight)
+character2Pos = character2X, character2Y = 324, 378 #screenWidth/5, 3*screenHeight/5
+character2 = pygame.Rect(character1X, character1Y, characterWidth, characterHeight)
+
 jumpDirection = -2
 jumped = 0
+
+
 
 #Enemy Creation
 enemySize = enemyWidth, enemyHeight = 20, 20
@@ -25,22 +31,22 @@ enemy = pygame.Rect(enemyX, enemyY, enemyWidth, enemyHeight)
 
 
 #Player Movement
-def playerMove(player):
-    global playerY, jumpDirection, jumped
+def characterMove(character):
+    global character1Y, jumpDirection, jumped
     key = pygame.key.get_pressed()
 
     #Jump
     if jumped:
-        player.move_ip(0, jumpDirection)
-        playerY += jumpDirection
-        if playerY == 218: #4/5th of the jump height
+        character1.move_ip(0, jumpDirection)
+        character1Y += jumpDirection
+        if character1Y == 218: #4/5th of the jump height
             if jumpDirection < 0:
-                jumpDirection = jumpDirection/2 #The player slows down as they reach the top of the jump
+                jumpDirection = jumpDirection/2 #The character slows down as they reach the top of the jump
             else:
-                jumpDirection *= 2              #The player speeds up coming from the top due to gravity.
-        if playerY == 178: #After 100 frames, the maximum height of the jump is reached.
+                jumpDirection *= 2              #The character speeds up coming from the top due to gravity.
+        if character1Y == 178: #After 100 frames, the maximum height of the jump is reached.
             jumpDirection *= -1
-        if playerY == 378: #After another 100 frames, the jump is finished
+        if character1Y == 378: #After another 100 frames, the jump is finished
             jumped = 0
     elif key[pygame.K_w]:
         jumpDirection = -2
@@ -57,10 +63,10 @@ while 1:
 
     surface.fill((0, 0, 125)) #Temporary background
 
-    playerMove(player)
+    characterMove(character1)
 
     pygame.draw.rect(surface, (0, 125, 0), battleground)
-    pygame.draw.rect(surface, (255, 255, 255), player)
+    pygame.draw.rect(surface, (255, 255, 255), character1)
     pygame.draw.rect(surface, (255, 0, 0), enemy)
 
     pygame.display.update(board)
