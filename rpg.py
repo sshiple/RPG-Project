a29c24728038a25fb9117b6fd08d14ac20c6cb10
sshiple <124829377+sshiple@users.noreply.github.com>
2023-05-01 21:00:29 -0400

    Added enemy phase and assigned sprites
    Each enemy takes its turn attacking the players

--- rpg.py
+++ rpg.py
@@ -11,7 +11,12 @@ pygame.display.set_caption("RPG")
 
 #Addition of images for the game
 background = pygame.image.load('bkg_image.png')
-mainCharacter = pygame.image.load('main_char_01.png')
+character1Sprite = pygame.image.load('main_char_left.png')
+character1Sprite = pygame.transform.flip(character1Sprite, True, False)
+character2Sprite = pygame.image.load('character_variants\main_char_02_left.png')
+character2Sprite = pygame.transform.flip(character2Sprite, True, False)
+enemy1Sprite = pygame.image.load('enemy_01_left.png')
+enemy2Sprite = pygame.image.load('character_variants\enemy_02_left.png')
 
 
 #Game Area
@@ -21,12 +26,11 @@ battleground = pygame.Rect(0, screenHeight/2, screenWidth, screenHeight)
 
 #Character Creation
 characterSize = characterWidth, characterHeight = 20, 20
-
-character1Pos = character1X, character1Y = 4*screenWidth/20, 3*screenHeight/5
+character1Pos = character1X, character1Y = 3*screenWidth/20, 3*screenHeight/5
 character1 = pygame.Rect(character1X, character1Y, characterWidth, characterHeight)
 character1Jumped = 0
 character1JumpDirection = -1
-character2Pos = character2X, character2Y = 3*screenWidth/20, 4*screenHeight/5
+character2Pos = character2X, character2Y = 5*screenWidth/20, 4*screenHeight/5
 character2 = pygame.Rect(character2X, character2Y, characterWidth, characterHeight)
 character2Jumped = 0
 character2JumpDirection = -1
@@ -34,7 +38,7 @@ character2JumpDirection = -1
 
 #Enemy Creation
 enemySize = enemyWidth, enemyHeight = 20, 20
-enemy1Pos = enemy1X, enemy1Y = (16*screenWidth/20) - enemyWidth, 3*screenHeight/5
+enemy1Pos = enemy1X, enemy1Y = (15*screenWidth/20) - enemyWidth, 3*screenHeight/5
 enemy1 = pygame.Rect(enemy1X, enemy1Y, enemyWidth, enemyHeight)
 enemy2Pos = enemy2X, enemy2Y = (17*screenWidth/20) - enemyWidth, 4*screenHeight/5
 enemy2 = pygame.Rect(enemy2X, enemy2Y, enemyWidth, enemyHeight)
@@ -42,18 +46,18 @@ enemy2 = pygame.Rect(enemy2X, enemy2Y, enemyWidth, enemyHeight)
 
 #Phases and Subphases
 playerPhase = 1 #If true, then it's the player phase. If false, then it's the enemy phase.
-enemy1Animation = 0 #If true,
+enemy1Animation = 1 #If true, then it's enemy1's turn to attack. If false, it's enemy2's turn.
+
 
 #FPS
 clock = pygame.time.Clock()
 
 
 #Player Movement
-def characterMove(char1, char2):
+def characterMove(char1, char2, dt):
     global character1Y, character1Jumped, character1JumpDirection, character2Y, character2Jumped, character2JumpDirection
     global playerPhase
     key = pygame.key.get_pressed()
-    dt = clock.tick(60) #The time that has passed since the last call to clock.tick(), capped at 60 fps
 
     if playerPhase:
         playerPhase = 0
@@ -73,7 +77,7 @@ def characterMove(char1, char2):
                     character1JumpDirection *= 2                        #The character speeds up coming from the top due to gravity.
             if char1.top <= character1Y - 200: #The maximum height of the jump is reached.
                 character1JumpDirection *= -1
-            if char1.top == character1Y: #The jump is finished
+            if char1.top >= character1Y: #The jump is finished.
                 character1Jumped = 0
 
         #Character2 Jump
@@ -90,24 +94,34 @@ def characterMove(char1, char2):
                     character2JumpDirection *= 2
             if char2.top <= character2Y - 200:
                 character2JumpDirection *= -1
-            if char2.top == character2Y:
+            if char2.top >= character2Y:
                 character2Jumped = 0
 
 
 #Enemy Movement
-def enemyMove(enem1, enem2):
-    global playerPhase
+def enemyMove(enem1, enem2, dt):
+    global playerPhase, enemy1Animation
 
     if not playerPhase:
         #Enemy1 Attack Animation
-
-        #attack based off rng
-        #after animation is done, then goes to enemy 2
-
-        #enemy2
-        #after animation is done, then player phase begins
-
-        playerPhase = 1
+        if enemy1Animation:
+            if enem1.left+enem1.width > 0:
+                enem1.move_ip(-1*dt, 0) #Temporary animation. The attack will be based off rng.
+                enem1.left -= dt
+            else:
+                enem1.update(enemy1X, enemy1Y, enemyWidth, enemyHeight)
+                enem1.left = enemy1X
+                enemy1Animation = 0 #After animation is done, the enemy2 animation begins
+        #Enemy2 Attack Animation
+        else:
+            if enem2.left+enem2.width > 0:
+                enem2.move_ip(-1*dt, 0)
+                enem2.left -= dt
+            else:
+                enem2.update(enemy2X, enemy2Y, enemyWidth, enemyHeight)
+                enem2.left = enemy2X
+                enemy1Animation = 1
+                playerPhase = 1 #After animation is done, the player phase begins
 
 
 #Game Loop
@@ -118,15 +132,21 @@ while 1:
             pygame.quit()
             sys.exit()
 
-    #surface.fill((0, 0, 125)) #Temporary background
+    deltaTime = clock.tick(60)/2 #The time that has passed since the last call to clock.tick(), capped at 60 fps. Divided by 2 to make things slower.
+
+    #Drawing Sprites
     surface.blit(background, (0,0)) #new background
-    surface.blit(mainCharacter,(character1.left-60, character1.top-100)) #
+    surface.blit(character1Sprite,(character1.left-60, character1.top-100))
+    surface.blit(character2Sprite,(character2.left-60, character2.top-100))
+    surface.blit(enemy1Sprite,(enemy1.left-60, enemy1.top-100))
+    surface.blit(enemy2Sprite,(enemy2.left-60, enemy2.top-100))
 
-    characterMove(character1, character2)
-    enemyMove(enemy1, enemy2)
+    #Movement Animations
+    characterMove(character1, character2, deltaTime)
+    enemyMove(enemy1, enemy2, deltaTime)
 
-    #pygame.draw.rect(surface, (0, 125, 0), battleground)
-    #pygame.draw.rect(surface, (255, 255, 255), character1)
+    #Hitboxes
+    pygame.draw.rect(surface, (255, 255, 255), character1)
     pygame.draw.rect(surface, (255, 255, 255), character2)
     pygame.draw.rect(surface, (255, 0, 0), enemy1)
     pygame.draw.rect(surface, (255, 0, 0), enemy2)
